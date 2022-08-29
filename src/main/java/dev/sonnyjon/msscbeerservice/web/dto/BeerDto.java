763702bc36edcdf88d4b093d6ccfbf3e335cd53e
sonnyjon <sonny.s.jon@gmail.com>
2022-08-29 16:35:54 -0700

    Added Jackson Configuration to BeerDto

--- src/main/java/dev/sonnyjon/msscbeerservice/web/dto/BeerDto.java
+++ src/main/java/dev/sonnyjon/msscbeerservice/web/dto/BeerDto.java
@@ -1,14 +1,12 @@
 package dev.sonnyjon.msscbeerservice.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -24,22 +22,34 @@ public class BeerDto
 {
     @Null
     private UUID id;
+
     @Null
     private Integer version;
+
     @NotBlank
     private String name;
+
     @NotNull
     private BeerStyle style;
+
     @Positive
     @NotNull
     private Long upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
+
+    @PositiveOrZero
     private Integer quantityOnHand;
+
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
 }
