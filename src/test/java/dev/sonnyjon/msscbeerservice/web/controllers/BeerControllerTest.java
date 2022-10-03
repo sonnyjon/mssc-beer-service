eceb2103e67f263ca82973851489cb0d8e890e01
sonnyjon <sonny.s.jon@gmail.com>
2022-10-03 04:45:25 -0700

    Tests for BeerService and BeerController

--- src/test/java/dev/sonnyjon/msscbeerservice/web/controllers/BeerControllerTest.java
+++ src/test/java/dev/sonnyjon/msscbeerservice/web/controllers/BeerControllerTest.java
@@ -1,11 +1,14 @@
 package dev.sonnyjon.msscbeerservice.web.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.sonnyjon.msscbeerservice.cases.TestBeerDto;
+import dev.sonnyjon.msscbeerservice.cases.TestConstants;
 import dev.sonnyjon.msscbeerservice.controllers.BeerController;
 import dev.sonnyjon.msscbeerservice.controllers.NotFoundException;
-import dev.sonnyjon.msscbeerservice.services.BeerService;
 import dev.sonnyjon.msscbeerservice.dto.BeerDto;
+import dev.sonnyjon.msscbeerservice.model.beer.BeerPagedList;
 import dev.sonnyjon.msscbeerservice.model.beer.BeerStyle;
+import dev.sonnyjon.msscbeerservice.services.BeerService;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockitoAnnotations;
@@ -13,16 +16,19 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -32,7 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(BeerController.class)
 class BeerControllerTest
 {
-    public static final String PATH_ROOT = "/api/v1/beer/";
+    public static final String PATH_ROOT = "/api/v1/";
 
     @Autowired
     MockMvc mockMvc;
@@ -58,30 +64,133 @@ class BeerControllerTest
     }
 
     @Nested
-    class BeerByIdTest
+    @DisplayName("listBeers() Method")
+    class ListBeersTest
     {
         @Test
-        @DisplayName("should return BeerDTO given valid UUID")
-        void givenValid_UUID_whenGetBeerById_thenBeerDto() throws Exception
+        @DisplayName("should return BeerPagedList for given beer name and style")
+        void givenBeerName_andStyle_andInventoryFlag_whenListBeers_thenBeerPagedList() throws Exception
         {
             // given
-            final BeerDto beerDto = getTestBeerDto();
-            final String PATH = PATH_ROOT + UUID.randomUUID();
+            final String PATH = PATH_ROOT + "beer";
+            BeerPagedList expected = getTestBeerPagedList();
 
             // when, then
-            when(beerService.getById( any(UUID.class), anyBoolean() )).thenReturn( beerDto );
+            when(beerService.listBeers(anyString(), any(BeerStyle.class), any(), anyBoolean()))
+                    .thenReturn( expected );
 
-            mockMvc.perform(get( PATH )
-                            .accept( MediaType.APPLICATION_JSON ))
-                    .andExpect( status().isOk() );
+            MvcResult result = mockMvc.perform(get( PATH )
+                            .accept( MediaType.APPLICATION_JSON )
+                            .param("pageNumber", "1")
+                            .param("pageSize", "1")
+                            .param("name", "Test Beer")
+                            .param("style", "IPA")
+                            .param("showInventoryOnHand", "false"))
+                    .andDo( print() )
+                    .andExpect( status().isOk() )
+                    .andReturn();
+
+            String jsonValue = result.getResponse().getContentAsString();
+            BeerPagedList actual = objectMapper.readValue( jsonValue, BeerPagedList.class );
+
+            assertNotNull( actual );
+            assertEquals( expected.getSize(), actual.getSize() );
+            assertEquals( expected.getPageable(), actual.getPageable() );
+        }
+    }
+
+    @Nested
+    @DisplayName("saveNewBeer() Method")
+    class SaveNewBeerTest
+    {
+        @Test
+        @DisplayName("should save beer info from BeerDto")
+        void givenBeerDto_whenSaveNewBeer_thenSaveBeer() throws Exception
+        {
+            // given
+            final String PATH = PATH_ROOT + "beer";
+            final BeerDto expected = TestBeerDto.getBeerDto();
+            final String beerContent = objectMapper.writeValueAsString( expected );
+
+            // when, then
+            when(beerService.saveNewBeer( any(BeerDto.class) )).thenReturn( expected );
+
+            MvcResult result = mockMvc.perform(post( PATH )
+                                                .contentType( MediaType.APPLICATION_JSON )
+                                                .content( beerContent ))
+                                        .andExpect( status().isCreated() )
+                                        .andReturn();
+
+            String jsonValue = result.getResponse().getContentAsString();
+            BeerDto actual = objectMapper.readValue( jsonValue, BeerDto.class );
+
+            assertNotNull( actual );
+            assertEquals( expected.getName(), actual.getName() );
+            assertEquals( expected.getStyle().name(), actual.getStyle().name() );
+            assertEquals( expected.getUpc(), actual.getUpc() );
+
+            verify(beerService, times(1)).saveNewBeer( any(BeerDto.class) );
+        }
+    }
+
+    @Nested
+    @DisplayName("getBeerById() Method")
+    class GetBeerByIdTest
+    {
+        @Test
+        @DisplayName("should return BeerDto with QoH for the given beer ID")
+        void givenBeerId_andInventoryFlag_whenGetBeerById_thenBeerDto() throws Exception
+        {
+            // given
+            final BeerDto expected = TestBeerDto.getBeerDtoWithQuantity();
+            final String PATH = PATH_ROOT + "beer/" + UUID.randomUUID();
+
+            // when, then
+            when(beerService.getById( any(UUID.class), anyBoolean() )).thenReturn( expected );
+
+            MvcResult result = mockMvc.perform(get( PATH )
+                                                .accept( MediaType.APPLICATION_JSON ))
+                                        .andExpect( status().isOk() )
+                                        .andReturn();
+
+            String jsonValue = result.getResponse().getContentAsString();
+            BeerDto actual = objectMapper.readValue( jsonValue, BeerDto.class );
+
+            assertNotNull( actual );
+            assertNotNull( actual.getQuantityOnHand() );
+            assertEquals( expected, actual );
         }
 
         @Test
-        @DisplayName("should throw NotFoundException when UUID invalid.")
-        void givenInvalid_UUID_whenGetBeerById_thenThrowException() throws Exception
+        @DisplayName("should return BeerDto without QoH for the given beer ID")
+        void givenBeerId_whenGetBeerById_thenBeerDto() throws Exception
         {
             // given
-            final String PATH = PATH_ROOT + UUID.randomUUID();
+            final BeerDto expected = TestBeerDto.getBeerDtoWithoutQuantity();
+            final String PATH = PATH_ROOT + "beer/" + UUID.randomUUID();
+
+            // when, then
+            when(beerService.getById( any(UUID.class), anyBoolean() )).thenReturn( expected );
+
+            MvcResult result = mockMvc.perform(get( PATH )
+                            .accept( MediaType.APPLICATION_JSON ))
+                    .andExpect( status().isOk() )
+                    .andReturn();
+
+            String jsonValue = result.getResponse().getContentAsString();
+            BeerDto actual = objectMapper.readValue( jsonValue, BeerDto.class );
+
+            assertNotNull( actual );
+            assertNull( actual.getQuantityOnHand() );
+            assertEquals( expected, actual );
+        }
+
+        @Test
+        @DisplayName("should throw NotFoundException: Bad beer ID")
+        void givenBadBeerId_whenGetBeerById_thenNotFoundException() throws Exception
+        {
+            // given
+            final String PATH = PATH_ROOT + "beer/" + UUID.randomUUID();
 
             // when, then
             when(beerService.getById( any(UUID.class), anyBoolean() )).thenThrow( NotFoundException.class );
@@ -94,69 +203,102 @@ class BeerControllerTest
         }
     }
 
-    @Test
-    void givenBeerDto_whenSaveNewBeer_thenSaveBeer() throws Exception
+    @Nested
+    @DisplayName("getBeerByUpc() Method")
+    class GetBeerByUpcTest
     {
-        // given
-        final BeerDto testBeer = getTestBeerDto();
-        final String beerContent = objectMapper.writeValueAsString( testBeer );
+        @Test
+        @DisplayName("should return BeerDto for the given UPC")
+        void givenUpc_whenGetBeerByUpc_thenBeerDto() throws Exception
+        {
+            // given
+            final String PATH = PATH_ROOT + "beerUpc/" + TestConstants.BEER_1_UPC;
+            BeerDto expected = TestBeerDto.getBeerDto();
 
-        // when, then
-        when(beerService.saveNewBeer( any(BeerDto.class) )).thenReturn( testBeer );
+            // when, then
+            when(beerService.getByUpc( anyString() )).thenReturn( expected );
 
-        mockMvc.perform(post(PATH_ROOT)
-                        .contentType( MediaType.APPLICATION_JSON )
-                        .content( beerContent ))
-                .andExpect( status().isCreated() );
+            MvcResult result = mockMvc.perform(get( PATH )
+                                            .accept( MediaType.APPLICATION_JSON ))
+                                    .andExpect( status().isOk() )
+                                    .andReturn();
 
-        verify(beerService, times(1)).saveNewBeer( any(BeerDto.class) );
+            String jsonValue = result.getResponse().getContentAsString();
+            BeerDto actual =  objectMapper.readValue( jsonValue, BeerDto.class );
+
+            assertNotNull( actual );
+            assertEquals( TestConstants.BEER_1_UPC, actual.getUpc() );
+
+            verify(beerService, times(1)).getByUpc( anyString() );
+        }
     }
 
-    @Test
-    void givenValid_UUIDandDto_whenUpdateBeerById_thenSaveBeer() throws Exception
+    @Nested
+    @DisplayName("updateBeerById() Method")
+    class UpdateBeerById
     {
-        // given
-        final BeerDto testBeer = getTestBeerDto();
-        final String beerContent = objectMapper.writeValueAsString( testBeer );
-        final String PATH = PATH_ROOT + UUID.randomUUID();
+        @Test
+        @DisplayName("should find Beer with the given ID and update Beer from data in DTO")
+        void givenBeerId_andBeerDto_whenUpdateBeerById_thenUpdateBeerFromDto() throws Exception
+        {
+            // given
+            final BeerDto expected = TestBeerDto.getBeerDto();
+            final String beerContent = objectMapper.writeValueAsString( expected );
+            final String PATH = PATH_ROOT + "beer/" + UUID.randomUUID();
 
-        // when, then
-        when(beerService.updateBeer( any(UUID.class), any(BeerDto.class) )).thenReturn( testBeer );
+            // when, then
+            when(beerService.updateBeer( any(UUID.class), any(BeerDto.class) )).thenReturn( expected );
 
-        mockMvc.perform(put( PATH )
-                        .contentType( MediaType.APPLICATION_JSON )
-                        .content( beerContent ))
-                .andExpect( status().isNoContent() );
+            MvcResult result = mockMvc.perform(put( PATH )
+                                                .contentType( MediaType.APPLICATION_JSON )
+                                                .content( beerContent ))
+                                        .andExpect( status().isNoContent() )
+                                        .andReturn();
 
-        verify(beerService, times(1)).updateBeer( any(UUID.class), any(BeerDto.class) );
+            String jsonValue =  result.getResponse().getContentAsString();
+            BeerDto actual = objectMapper.readValue( jsonValue, BeerDto.class );
+
+            assertNotNull( actual );
+            assertEquals( expected.getName(), actual.getName() );
+            assertEquals( expected.getStyle().name(), actual.getStyle().name() );
+            assertEquals( expected.getUpc(), actual.getUpc() );
+
+            verify(beerService, times(1)).updateBeer( any(UUID.class), any(BeerDto.class) );
+        }
+
+        @Test
+        @DisplayName("should throw NotFoundException: Bad customer ID")
+        void givenBadBeerId_whenUpdateBeerBeerById_thenNotFoundException() throws Exception
+        {
+            // given
+            final String beerContent = objectMapper.writeValueAsString( TestBeerDto.getBeerDto() );
+            final String PATH = PATH_ROOT + "beer/" + UUID.randomUUID();
+
+            // when, then
+            when(beerService.updateBeer( any(UUID.class), any(BeerDto.class) )).thenThrow( NotFoundException.class );
+
+            mockMvc.perform(put( PATH )
+                            .contentType( MediaType.APPLICATION_JSON )
+                            .content( beerContent ))
+                    .andExpect( status().isNotFound() )
+                    .andExpect(result -> assertTrue(result.getResolvedException() instanceof NotFoundException));
+
+            verify(beerService, times(1)).updateBeer( any(UUID.class), any(BeerDto.class) );
+        }
     }
 
-    @Test
-    void givenInvalid_UUID_whenUpdateBeerById_thenThrowException() throws Exception
+    //==================================================================================================================
+    private BeerPagedList getTestBeerPagedList()
     {
-        // given
-        final String beerContent = objectMapper.writeValueAsString( getTestBeerDto() );
-        final String PATH = PATH_ROOT + UUID.randomUUID();
+        List<BeerDto> content = List.of( getTestBeerDto() );
+        PageRequest pageRequest = PageRequest.of( 1, 1 );
 
-        // when, then
-        when(beerService.updateBeer( any(UUID.class), any(BeerDto.class) )).thenThrow( NotFoundException.class );
-
-        mockMvc.perform(put( PATH )
-                        .contentType( MediaType.APPLICATION_JSON )
-                        .content( beerContent ))
-                .andExpect( status().isNotFound() )
-                .andExpect(result -> assertTrue(result.getResolvedException() instanceof NotFoundException));
-
-        verify(beerService, times(1)).updateBeer( any(UUID.class), any(BeerDto.class) );
+        return new BeerPagedList(content, pageRequest, 1);
     }
 
     private BeerDto getTestBeerDto()
     {
-        return BeerDto.builder()
-                .name( "My Beer" )
-                .style( BeerStyle.ALE )
-                .price(new BigDecimal( "2.99" ))
-//                .upc( BeerDataLoader.BEER_1_UPC )
-                .build();
+        return TestBeerDto.getBeerDto();
     }
+
 }
