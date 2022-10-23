58026c4f218f2252e8d994c62079e8e22470a0ae
sonnyjon <sonny.s.jon@gmail.com>
2022-10-23 09:27:54 -0700

    Removed "exclude" clause from @SpringBootApplication annotation so that jmsTemplate can be found.

--- src/main/java/dev/sonnyjon/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/dev/sonnyjon/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,11 +2,10 @@ package dev.sonnyjon.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication
 public class MsscBeerServiceApplication
 {
 
